21efe4bc5df12abfa9aa2a3c202280505d6dbe7a
tolga <tolgagobel28@hotmail.com>
2024-01-25 17:15:41 +0300

    last update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import 'react-native-gesture-handler'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import CategoryScreen from './screens/CategoryScreen'
 import FoodOverviewScreen from './screens/FoodOverviewScreen'
 import FoodScreen from './screens/FoodScreen'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Favorites from './screens/Favorites'
 
 export default function App() {
     const Stack = createNativeStackNavigator()
-  return (
-   <NavigationContainer>
-    <Stack.Navigator screenOptions={{contentStyle:{backgroundColor:'#fff'}}}>
-        <Stack.Screen options={({headerTitle:'Tüm Kategoriler'})} name='Category' component={CategoryScreen}/>
-        <Stack.Screen name='FoodOverview' component={FoodOverviewScreen}/>
-        <Stack.Screen name='Food' component={FoodScreen}/>
-    </Stack.Navigator>
-   </NavigationContainer>
-  )
+    const Drawer = createDrawerNavigator();
+
+    function DrawerNavigation() {
+        return (
+            <Drawer.Navigator>
+                <Drawer.Screen options={({ headerTitle: 'Tüm Kategoriler' })} name="Category" component={CategoryScreen} />
+                <Drawer.Screen name="Favorites" component={Favorites} />
+            </Drawer.Navigator>
+        );
+    }
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ contentStyle: { backgroundColor: '#fff' } }}>
+                <Stack.Screen options={{headerShown:false}} name='Drawer' component={DrawerNavigation} />
+                <Stack.Screen name='FoodOverview' component={FoodOverviewScreen} />
+                <Stack.Screen
+                    options={{
+                        headerRight: () => (
+                            <TouchableOpacity>
+                                <AntDesign name="staro" size={20} />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                    name='Food'
+                    component={FoodScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
 const styles = StyleSheet.create({})
